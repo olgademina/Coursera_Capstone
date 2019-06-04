3cc36608d1db94c05f8e60e7f320ee5b295e7d6d
Olga Demina <Olga.Demina@acronis.com>
2019-06-04 21:02:21 +0300

    adds python script and Applied Data Science Capstone notebook

---
+++ capstone.py
@@ -0,0 +1,19 @@
+
+# coding: utf-8
+
+# <h1 align="center"><font size="5">Applied Data Science Capstone</font></h1>
+
+# This notebook will be mainly used for the Coursera IBM Applied Data Science Capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
